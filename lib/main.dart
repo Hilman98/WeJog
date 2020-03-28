44f961deee2fc4c55e00251af68b319ca54a8b41
Hilman98 <62791261+Hilman98@users.noreply.github.com>
2020-03-28 17:52:52 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:wejog1/registrationForm.dart';
-import 'package:wejog1/dashboard.dart';
+import 'dart:async';
+import 'package:wejog1/login.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     home: Home(),
+    
+     home: new SplashScreen(),
+     routes: <String, WidgetBuilder>{
+        '/Login': (BuildContext context) => new Login()
+    },
     );
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
 
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 5);
+    return new Timer(_duration, navigationPage);
+  }
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/Login');
+  }
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green[500],
-      body: Container(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            
-            Image.asset('assets/logo.png'),
-            SizedBox(
-              height: 50
-            ),
-            
-            TextField(
-              style: TextStyle(
-                color: Colors.white,
-              ),
-                decoration: InputDecoration(
-                 enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                ) ,
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color:  Colors.white,
-                    width: 2.0
-                  )
-                ),
-                labelText: 'Username',
-                labelStyle: TextStyle(
-                  color: Colors.white,
-                  
-                ),
-                hintText: 'Enter your username',
-                hintStyle: TextStyle(
-                  color: Colors.white
-                )
-              ),
-            ),
-             SizedBox(
-              height: 20
-            ),
-            
-            TextField(
-              style: TextStyle(
-                color: Colors.white,
-              ),
-              decoration: InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                ) ,
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color:  Colors.white,
-                    width: 2.0
-                  )
-                ),
-                labelText: 'Password',
-                labelStyle: TextStyle(
-                  color: Colors.white
-                ),
-                hintText: 'Enter your password',
-                hintStyle: TextStyle(
-                  color: Colors.white
-                )
-              ),
-            ),
-            SizedBox(
-                width: double.infinity,
-                child: RaisedButton.icon(
-                onPressed: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Dashboard()),
-                  );
-                }, 
-                icon: Icon(Icons.exit_to_app), 
-                label: Text("Login"),
-                color: Colors.blue,
-                textColor: Colors.white,
-                
-              ),
-            ),
-            SizedBox(
-                width: double.infinity,
-                child: RaisedButton.icon(
-                onPressed: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => RegistrationForm()),
-            );
-
-                }, 
-                icon: Icon(Icons.account_circle), 
-                label: Text("Create an account"),
-                color: Colors.amber,
-              ),
-            )
-            
+            Image.asset("assets/run.gif"),
+            Image.asset("assets/logoSmall.png"),
           ],
-        ),
-      )
+        )
+      ),
     );
   }
 }
-
